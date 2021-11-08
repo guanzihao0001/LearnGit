8a42b1ae4a2dd1a15a2ece8e03934bea90b97070
Dragon <1033871795@qq.com>
2021-11-08 13:30:12 +0100

    nothing

--- Ex4.py
+++ Ex4.py
@@ -8,9 +8,7 @@ stv=int(input())
 print("enter the end value:",end="")
 edv=int(input())
 
-
-print("")
-if T== "F":
+def celsius_to_fahrenheit(stv,edv):
     print("Celsius Farenheit")
     if stv>edv:
         for i in range(stv,edv-1,-1):
@@ -18,11 +16,18 @@ if T== "F":
     else:
         for i in range(stv,edv):
             print("{:^10d}".format(i), "{:^.2f}".format(1.8 * i + 32))
-elif T=="C":
+
+def fahrenheit_to_celsius(stv,edv):
     print("Farenheit Celsius")
     if stv > edv:
         for i in range(stv, edv - 1, -1):
-            print("{:^10d}".format(i), "{:^.2f}".format((i - 32)/1.8))
+            print("{:^10d}".format(i), "{:^.2f}".format((i - 32) / 1.8))
     else:
         for i in range(stv, edv):
             print("{:^10d}".format(i), "{:^.2f}".format((i - 32) / 1.8))
+
+print("")
+if T== "F":
+    celsius_to_fahrenheit(stv,edv)
+elif T=="C":
+    fahrenheit_to_celsius(stv,edv)
